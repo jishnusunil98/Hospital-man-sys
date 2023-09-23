233dc03c84b13499199dcdc5d70ea7bd1eaad307
Jishnu Sunil <jishnutvm98@gmail.com>
2023-09-23 19:27:28 +0530

    Path setup and allingement fix

--- hos_man_sys/app/urls.py
+++ hos_man_sys/app/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import include, path
 
 urlpatterns = [
-    path('',views.index),
-    path('about',views.about),
-    path('appointment',views.appointment),
-    path('doctors',views.doctors),
-    path('contact',views.contact),
+    path('',views.home,name="home"),
+    path('about',views.about,name="about"),
+    path('appointment',views.appointment,name="appointment"),
+    path('doctors',views.doctors,name="doctors"),
+    path('contact',views.contact,name="contact"),
 ]
